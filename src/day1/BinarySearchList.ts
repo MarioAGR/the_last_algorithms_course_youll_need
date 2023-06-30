c1bc4382c2722b914a2eeafaeb142f4bdca0a1d1
Mario Guillen <marioagr95@gmail.com>
2023-06-29 18:14:26 -0600

    Implementing Binary Search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lowest = 0;
+    let highest = haystack.length;
+    do {
+        const middle = Math.floor(lowest + (highest - lowest) / 2);
+        const value = haystack[middle];
+        if (value === needle) {
+            return true;
+        } else if (value > needle) {
+            highest = middle;
+        } else {
+            lowest = middle + 1;
+        }
+    } while (lowest < highest);
 
+    return false;
 }
