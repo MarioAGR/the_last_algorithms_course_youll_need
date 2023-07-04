0ef2ba956a146170d3f5c51ae9e57e2bc392762e
Mario Guillen <marioagr95@gmail.com>
2023-07-03 19:12:07 -0600

    Implementing QuickSort

--- src/day3/QuickSort.ts
+++ src/day3/QuickSort.ts
@@ -1,3 +1,37 @@
-export default function quick_sort(arr: number[]): void {
+function qs(arr: number[], lo: number, hi: number): void {
+    if (lo >= hi) {
+        return;
+    }
 
+    const pivotIndex = partition(arr, lo, hi);
+
+    qs(arr, lo, pivotIndex - 1);
+    qs(arr, pivotIndex + 1, hi);
+}
+
+function partition(arr: number[], lo: number, hi: number): number {
+    const pivot = arr[hi];
+
+    let index = lo - 1;
+
+    for (let i = lo; i < hi; i++) {
+        if (arr[i] <= pivot) {
+            index++;
+            // const tmp = arr[i];
+            // arr[i] = arr[index];
+            // arr[index] = tmp;
+            [arr[i], arr[index]] = [arr[index], arr[i]];
+        }
+    }
+
+    index++;
+    // arr[hi] = arr[index];
+    // arr[index] = pivot;
+    [arr[hi], arr[index]] = [arr[index], pivot];
+
+    return index;
+}
+
+export default function quick_sort(arr: number[]): void {
+    qs(arr, 0, arr.length - 1);
 }
